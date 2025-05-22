7adccf5b0085df35cfb308ed8d371455f39dc681
Raul <raulramirez.glez00@gmail.com>
2025-05-22 13:29:57 -0600

    Se agregaron los comentarios a las modificaciones del codigo y del html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = baseEndpoint + '/users';
+
+// Selección básica de elementos usando className
 const nameElement = document.querySelector('.name');
 const blogElement = document.querySelector('.blog');
 const locationElement = document.querySelector('.location');
@@ -7,21 +9,23 @@ const locationElement = document.querySelector('.location');
 function displayUser(username) {
   nameElement.textContent = 'Cargando...';
 
+  // Petición básica con fetch y promesas para el usuario
   fetch(usersEndpoint + '/' + username)
     .then(function(response) {
-      return response.json(); 
+      return response.json(); // Convertir a JSON los elementos
     })
     .then(function(data) {
-     
+      // Muestra los datos en el HTML
       nameElement.textContent = data.name || 'No hay nombre';
       blogElement.textContent = data.blog || 'No hay blog';
       locationElement.textContent = data.location || 'No hay ubicación';
     })
     .catch(function(error) {
-     
+      // Muestra si hay un error simple
       console.log('Ocurrió un error:', error);
       nameElement.textContent = 'Error al cargar usuario';
     });
 }
 
+// Llamada inicial
 displayUser('stolinski');
